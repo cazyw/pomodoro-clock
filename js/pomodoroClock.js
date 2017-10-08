af8f37e75f22b0c67c8e5ceeadf74548cfc7c840
cazyw <kazeisc@gmail.com>
2017-10-08 15:23:40 +1100

    Create SVG circle and path

--- js/pomodoroClock.js
+++ js/pomodoroClock.js
@@ -1,11 +1,21 @@
 const fillButton = () => {
-  let canvas = document.getElementById("drawing").classList
-  if (canvas.contains("drawing")) {
-    canvas.remove("drawing")
+  let path_rect = document.getElementsByTagName("path")[0].classList
+  let path_circ = document.getElementsByTagName("path")[1].classList
+  if (path_rect.contains("drawing") && path_circ.contains("drawing")) {
+    path_rect.remove("drawing")
+    path_circ.remove("drawing")
+    document.getElementById("time-set").innerHTML = "unset"
   } else {
     let time = prompt("Time in seconds: ")
-    let rect = document.querySelector("#drawing")
+    let rect = document.querySelector("#rect-drawing")
+    let circ = document.querySelector("#circ-drawing")
+    document.getElementById("time-set").innerHTML = `${time} seconds`
     rect.style.setProperty("--my-transition-time", `${time}s`)
-    canvas.add("drawing")
+    circ.style.setProperty("--my-transition-time", `${time}s`)
+    path_rect.add("drawing")
+    path_circ.add("drawing")
   }
+  let myPath = document.getElementById("circ-drawing");
+  var length = myPath.getTotalLength();
+  console.log(length);
 }
