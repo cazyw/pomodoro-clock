163194ac4f6b94db26ad7605fe91c61d4849abb2
cazyw <cazyw@users.noreply.github.com>
2017-10-09 21:30:56 +1100

    Add timer

--- js/pomodoroClock.js
+++ js/pomodoroClock.js
@@ -1,3 +1,9 @@
+/* eslint-disable */
+
+let sessionTime = 900
+let breakTime = 300
+let timer
+
 const fillButton = () => {
   let pathCirc = document.getElementsByTagName("path")[0].classList
   if (pathCirc.contains("drawing")) {
@@ -7,27 +13,33 @@ const fillButton = () => {
     // let time = prompt("Time in seconds: ")
     let circ = document.querySelector("#circ-drawing")
     let time = parseInt(document.getElementById("session-time").innerText) * 60
+    sessionTime = time
     document.getElementById("time-set").innerHTML = `${time / 60} minutes ${time % 60} seconds`
     circ.style.setProperty("--my-transition-time", `${time}s`)
     pathCirc.add("drawing")
-    setInterval(countDown, 1000);
+    timer = setInterval(countDown, 1000);
   }
 }
 
 const countDown = () => {
-  
+  if (sessionTime === 0) {
+    clearInterval(timer)
+  }
+  sessionTime -= 1;
+  document.getElementById("time-set").innerHTML = `${Math.floor(sessionTime / 60)} minutes ${Math.floor(sessionTime % 60)} seconds` 
+  console.log(sessionTime)
 }
 
 const changeTime = (id) => {
   console.log(id);
   if (id === "break-minus") {
-    if (parseInt(document.getElementById("break-time").innerText) > 0) {
+    if (parseInt(document.getElementById("break-time").innerText) > 1) {
       document.getElementById("break-time").innerHTML = parseInt(document.getElementById("break-time").innerText) - 1
     }
   } else if (id === "break-plus") {
       document.getElementById("break-time").innerHTML = parseInt(document.getElementById("break-time").innerText) + 1
   } else if (id === "session-minus") {
-    if (parseInt(document.getElementById("session-time").innerText) > 0) {
+    if (parseInt(document.getElementById("session-time").innerText) > 1) {
       document.getElementById("session-time").innerHTML = parseInt(document.getElementById("session-time").innerText) - 1
     }
   } else if (id === "session-plus") {
